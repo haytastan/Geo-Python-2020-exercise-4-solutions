0a801e53a960c2ce0d273f9c401dde7ac561393d
haytastan <haytastan@gmail.com>
2021-09-02 11:25:07 +0300

    update

--- Exercise-4-solution-1.py
+++ Exercise-4-solution-1.py
@@ -1,11 +1,11 @@
 # Hayati TAŞTAN
-# 01.09.2021
+# 02.09.2021
 # ===================
-#SUBJECT: Simple temperature calculator 
-# PROBLEM: https://github.com/haytastan/Geo-Python-2020-exercise-3-solutions/blob/main/Exercise-3-problem-1.ipynb
+# SUBJECT: Simple temperature calculator 
+# PROBLEM: https://github.com/haytastan/Geo-Python-2020-exercise-3-solutions/blob/main/Exercise-4-problem-1.ipynb
 # SOLUTION: 
 
-# pip install nose.tools
+# P.A.: pip install nose.tools
 
 from nose.tools import ok_, assert_equal
 import inspect
@@ -13,7 +13,7 @@ import inspect
 def fahr_to_celsius(temp_fahrenheit):
     return (temp_fahrenheit - 32) * (5/9)
 
-print('32 degrees Fahrenheit in Celsius is: ', fahr_to_celsius(32))
+
 
 
 # Check that the function has a single parameter and the parameter name is correct
@@ -23,3 +23,11 @@ if ok_(params[0]=='temp_fahrenheit') and ok_(len(params)==1):
     print('The function\'s arameter name is incorrect and the function has not a single parameter')
 else:
      print('The function\'s arameter name is correct and the function has a single parameter')
+        
+# 1. What is 48° Fahrenheit in Celsius? 
+print('48 degrees Fahrenheit in Celsius is {0:.2f}'.format(fahr_to_celsius(48)))
+
+# 2. What about 71° Fahrenheit in Celsius?
+print('71 degrees Fahrenheit in Celsius is {0:.2f}'.format(fahr_to_celsius(71)))
+
+
